159c8f51867ac3dab1231ac7c21abe4aa2e23dac
Allen <liupengliang1012@gmail.com>
2022-08-24 20:16:58 +0800

    2022

--- js/index.js
+++ js/index.js
@@ -11,8 +11,10 @@ $(document).ready(function() {
         $('.tag-panel').addClass('center');
     } else {
         var en = ['Home', 'Work Experience', 'Open source project', 'Solve difficult problems',
-                'Independently completed architecture deployment', 'Contact Me'];
-        var cn = ['首页', '工作经历', '开源项目', '解决的疑难问题', '独立完成的架构部署', '联系'];
+                'Distributed architecture completed independently in 2019',
+                'Physical topology of highly available distributed architecture in 2022',
+                'Logic topology of highly available distributed architecture in 2022', 'Contact Me'];
+        var cn = ['首页', '工作经历', '开源项目', '解决的疑难问题', '2019年独立完成的分布式架构', '2022年高可用分布式架构物理拓扑图', '2022年高可用分布式架构逻辑拓扑图', '联系'];
         var isCn = window.location.href.indexOf('cn.html') >= 0;
         $('.section').addClass('initing');
         $('#pagepiling').pagepiling({
